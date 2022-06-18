a95fb6f06f4a7014c955f1eaf863d6fb2b6187fd
IrvingArmenta <irvingarmentajd@gmail.com>
2022-06-18 19:57:44 +0900

    feat: styled-components and eslint setup

---
+++ src/global-styles/themes/dark.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from 'styled-components';
+
+const darkTheme: DefaultTheme = {
+  colors: {
+    main: '#2f2d34',
+    primary: 'red',
+    secondary: 'magenta',
+  },
+  neuphorm: {
+    boxShadow: `9px 9px 33px #26242a,
+    -9px -9px 33px #38363e;`,
+    borderRadius: '12px',
+  },
+};
+
+export default darkTheme;
---
+++ src/global-styles/themes/default.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from 'styled-components';
+
+const defaultTheme: DefaultTheme = {
+  colors: {
+    main: '#dedede',
+    primary: 'red',
+    secondary: 'magenta',
+  },
+  neuphorm: {
+    boxShadow: `9px 9px 33px #b2b2b2,
+      -9px -9px 33px #ffffff;`,
+    borderRadius: '12px',
+  },
+};
+
+export default defaultTheme;
